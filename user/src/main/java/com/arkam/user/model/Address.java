d7c69b56fabe63c454d26768d8c7b6d588e469db
Freddy <freddy.artea@gmail.com>
2025-06-18 22:09:01 -0500

    Se ajusta usuarios para trabajar con MongoDB Atlas Cloud

--- user/src/main/java/com/arkam/user/model/Address.java
+++ user/src/main/java/com/arkam/user/model/Address.java
@@ -1,15 +1,11 @@
 package com.arkam.user.model;
 
-import jakarta.persistence.*;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@Entity(name = "addresses")
 public class Address {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String street;
     private String city;
