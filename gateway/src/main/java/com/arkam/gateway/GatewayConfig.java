4fc7511c4565f443152f3bcb3c76f06719f1f4da
Freddy <freddy.artea@gmail.com>
2025-08-18 11:31:25 -0500

    Configurtando Ratelimitign en GatWay

--- gateway/src/main/java/com/arkam/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/arkam/gateway/GatewayConfig.java
@@ -1,14 +1,28 @@
 package com.arkam.gateway;
 
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewayConfig {
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(10,20,1);
+    }
+
+    @Bean
+    public KeyResolver hostNameKeyResolver() {
+        return exchange -> Mono.just(
+                exchange.getRequest().getRemoteAddress().getHostName());
+    }
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
@@ -19,8 +33,11 @@ public class GatewayConfig {
                                         .setRetries(10)
                                         .setMethods(HttpMethod.GET)
                                 )
+                                .requestRateLimiter(config -> config
+                                        .setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(hostNameKeyResolver()))
                                 .circuitBreaker(config -> config
-                                .setName("ecomBreaker")
+                                .setName("ecomArkam")
                                 .setFallbackUri("forward:/fallback/products")))
 //                        .filters(f -> f.rewritePath("/products(?<segment>/?.*)",
 //                                "/api/products${segment}"))
